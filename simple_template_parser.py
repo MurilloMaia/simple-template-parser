928aafec800ec9c241857c8e98cdb03efe97b058
Murillo Maia <peo_mmaia>
2016-09-21 20:12:57 -0300

    update to use csv

--- simple_template_parser.py
+++ simple_template_parser.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 import json
+import csv
 import codecs
 import argparse
 import textwrap
@@ -26,12 +27,19 @@ def main():
             -----------------------------------
 
 
-            File format:
+            JSON file format:
             -----------------------------------
             [
                 {"KEY": "VALUE"},
                 {"KEY": "OTHER_VALUE"}
             ]
+            -----------------------------------
+
+            CSV file format:
+            -----------------------------------
+            KEY\tKEY2
+            VALUE_KEY\tVALUE_KEY2
+            OTHER_VALUE_KEY\tOTHER_VALUE_KEY2
             -----------------------------------
 
 
@@ -46,19 +54,20 @@ def main():
         )
 
     parser.add_argument('-t', '--template', help='template file to process, with keys to replace the value', required=True)
-    parser.add_argument('-te', '--template-encoding', help='template file encoding', default='UTF-8')
+    parser.add_argument('-te', '--template-encoding', help='template file encoding, default=UTF-8', default='UTF-8')
     parser.add_argument('-d', '--data', help='data file to process, with a json array', required=True)
-    parser.add_argument('-de', '--data-encoding', help='data file encoding', default='UTF-8')
+    parser.add_argument('-de', '--data-encoding', help='data file encoding, default=UTF-8', default='UTF-8')
+    parser.add_argument('-dt', '--data-type', help='data file type, defaul=json', default='json')
+    parser.add_argument('-dtcsvd', '--data-type-csv-delimiter', help='csv data type delimiter, default=\\t', default='\t')
     parser.add_argument('-iter', '--iterate', help='When iterate over data file, needs to press a key to continue', action='store_true')
-    parser.add_argument('-o', '--output', help='path to output file')
-    parser.add_argument('-oe', '--output-encoding', help='output file encoding', default='UTF-8')
+    parser.add_argument('-o', '--output', help='path to output file, prints the output only in the file')
+    parser.add_argument('-oe', '--output-encoding', help='output file encoding, default=UTF-8', default='UTF-8')
 
     args = parser.parse_args()
     
     template = Template(codecs.open(args.template, READ, args.template_encoding).read())
 
-    with codecs.open(args.data, READ, args.data_encoding) as f:
-        data = json.load(f)
+    data = parseData(args)
 
     if args.output:
         global output
@@ -68,6 +77,29 @@ def main():
         write(process_template(template, **p), args.iterate)
 
     printSuccess()
+
+def parseData(args):
+    parsers = {
+        "json" : parseJson,
+        "csv" : parseCsv
+    }
+    with codecs.open(args.data, READ, args.data_encoding) as file:
+        return parsers[args.data_type](args, file)
+
+def parseJson(args, file):
+    return json.load(file)
+
+def parseCsv(args, file):
+    reader = csv.DictReader(file, delimiter=args.data_type_csv_delimiter)
+    data = []        
+    for row in reader:
+        temp = {}
+        for fieldname in reader.fieldnames:
+            temp[fieldname] = row[fieldname]
+
+        data.append(temp)
+
+    return data
 
 def write(parsed_content, iterate):
     if output:
